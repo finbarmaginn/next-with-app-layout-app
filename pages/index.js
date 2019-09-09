b67ac04eaea3960cdb6d9f59c09e8ff0e3623c8e
Finbar Maginn <MAGINF01@DIXONSRETAIL.COM>
2019-09-09 16:11:53 +0100

    remove react parallax

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import ParallaxContent from "../components/ParallaxContent";
-
 export default () => {
   return (
     <div>
       <div className="wrapper">
         <h2>Home</h2>
       </div>
-      <ParallaxContent />
     </div >
   )
 }
